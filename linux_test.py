7829913ac016610fdcedebd0cc93c1b3d3e892b4
CallousOverlord <kht6@duke.edu>
2015-02-13 22:57:39 -0500

    linux test

---
+++ linux_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This temporary script file is located here:
+/home/kyle/.spyder2/.temp.py
+"""
+
+import numpy
+import pandas
+
+test test... does this IDE and Git account work?
